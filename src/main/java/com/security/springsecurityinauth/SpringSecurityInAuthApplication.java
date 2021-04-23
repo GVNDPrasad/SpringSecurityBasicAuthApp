2b27877378dee7d7247a42d5148459d0b61ff176
DELL <DELL@DESKTOP-9VFNF4E>
2021-04-23 13:04:51 +0530

    Base commit

---
+++ src/main/java/com/security/springsecurityinauth/SpringSecurityInAuthApplication.java
@@ -0,0 +1,22 @@
+package com.security.springsecurityinauth;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Spring Security application for User-defined/Basic Security Authentication
+
+@SpringBootApplication
+public class SpringSecurityInAuthApplication {
+    
+	private static final Logger log = LoggerFactory.getLogger(SpringSecurityInAuthApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityInAuthApplication.class, args);
+		log.info("SpringSecurityInAuthApplication :: main()");
+		
+		System.out.println("Use the credentials from application.properties file for Username: ? and Password:?");
+	}
+
+}
